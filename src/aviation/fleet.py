f8de32534015066e90aac914e50e3b178b6addb0
Samuel Gabra <sg788@cam.ac.uk>
2025-07-22 12:12:29 +0100

    Add docstrings to functions in src

--- src/aviation/fleet.py
+++ src/aviation/fleet.py
@@ -1,6 +1,24 @@
+"""Modelling of the global fleet based on average passenger and aircraft data."""
+
+
 def passengers_per_day(passengers_per_year, days_per_year):
+    """The number of passengers per day globally.
+
+    Args:
+        passengers_per_year (float): The number of passengers flying per year globally.
+        days_per_year (float): The number of days in the modelled year.
+
+    """
     return passengers_per_year / days_per_year
 
 
 def required_global_fleet(passengers_per_day, seats_per_aircraft, flights_per_aircraft_per_day):
+    """The size of the required global fleet.
+
+    Args:
+        passengers_per_day (float): The number of passengers flying per year globally.
+        seats_per_aircraft (float): The number of seats in the modelled aircraft.
+        flights_per_aircraft_per_day (float): The number of flights per aircraft per day
+
+    """
     return passengers_per_day / (seats_per_aircraft * flights_per_aircraft_per_day)
