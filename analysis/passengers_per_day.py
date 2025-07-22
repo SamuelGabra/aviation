ad064a1f3f2320fcf625033ac8e3efa51c745092
Samuel Gabra <sg788@cam.ac.uk>
2025-07-22 17:28:12 +0100

    Add transform decorator to functions

--- analysis/passengers_per_day.py
+++ analysis/passengers_per_day.py
@@ -4,8 +4,6 @@ import aviation
 
 days_per_year = 365.0
 passengers_per_year = 6_000_000_000.0
-seats_per_aircraft = 200.0
-flights_per_aircraft_per_day = 3.0
 
 passengers_per_day = aviation.passengers_per_day(
     passengers_per_year=passengers_per_year, days_per_year=days_per_year
