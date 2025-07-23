0149aae997c847e6f117c76f4e5c6729c3429894
Samuel Gabra <sg788@cam.ac.uk>
2025-07-23 11:58:03 +0100

    Add systems model requirements

--- analysis/required_global_fleet.py
+++ analysis/required_global_fleet.py
@@ -1,19 +1,21 @@
 """Analysis to determine the number of aircrafts required in a global fleet."""
 
 import aviation
+from aviation import _engine as engine
 
 days_per_year = 365.0
 passengers_per_year = 6_000_000_000.0
 seats_per_aircraft = 200.0
 flights_per_aircraft_per_day = 3.0
 
-passengers_per_day = aviation.passengers_per_day(
-    passengers_per_year=passengers_per_year, days_per_year=days_per_year
-)
-global_fleet_size = aviation.required_global_fleet(
-    passengers_per_day=passengers_per_day,
-    seats_per_aircraft=seats_per_aircraft,
-    flights_per_aircraft_per_day=flights_per_aircraft_per_day,
-)
+inputs = {
+    "passengers_per_year": passengers_per_year,
+    "days_per_year": days_per_year,
+    "seats_per_aircraft": seats_per_aircraft,
+    "flights_per_aircraft_per_day": flights_per_aircraft_per_day,
+}
+output = "required_global_fleet"
 
-print(f"The global fleet size is: {global_fleet_size}")
+systems_model = engine.SystemsModel(aviation.transforms)
+required_global_fleet = systems_model.evaluate(inputs, output)
+print(f"The required global fleet is: {required_global_fleet}")
